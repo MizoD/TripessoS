6d35f5edf33157ac7aa6fb14f9712872c43e178e
AhmedAdel1998 <54945112+AhmedAdel1998@users.noreply.github.com>
2025-08-14 14:19:35 +0300

    AdminDashBoard

--- Tripesso/Areas/Admin/HomeDataController.cs
+++ Tripesso/Areas/Admin/HomeDataController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Tripesso.Areas.Admin
 {
@@ -9,5 +11,34 @@ namespace Tripesso.Areas.Admin
     [ApiController]
     public class HomeDataController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeDataController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("index")]
+        public async Task<IActionResult> GetDashboardStats()
+        {
+            var bookingsCountTask = _context.Bookings.CountAsync();
+            var tripsCountTask = _context.Trips.CountAsync();
+            var flightsCountTask = _context.Flights.CountAsync();
+            var hotelsCountTask = _context.Hotel.CountAsync();
+            var totalBookingsMadeTask = _context.Bookings.SumAsync(b => (decimal?)b.TotalAmount ?? 0);
+
+            await Task.WhenAll(bookingsCountTask, tripsCountTask, flightsCountTask, hotelsCountTask, totalBookingsMadeTask);
+
+            var result = new
+            {
+                BookingsCount = bookingsCountTask.Result,
+                TripsCount = tripsCountTask.Result,
+                FlightsCount = flightsCountTask.Result,
+                HotelsCount = hotelsCountTask.Result,
+                TotalBookingsMade = totalBookingsMadeTask.Result
+            };
+
+            return Ok(result);
+        }
     }
 }
